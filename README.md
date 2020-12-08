0b8e0622d732cfdce47119a63c9365d29a9c6bcd
Thu Nguyen <thu.nguyen@vudu.com>
2020-12-07 16:55:28 -0800

    Completed day 6

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ To save your vacation, you need to get all fifty stars by December 25th.
 
 Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 
-* [Day 1](https://github.com/thunguyen19/advent-of-code-2020/day1)
-* [Day 2](https://github.com/thunguyen19/advent-of-code-2020/day2)
+* [Day 1](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day1)
+* [Day 2](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day2)
+* [Day 3](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day3)
+* [Day 4](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day4)
+* [Day 5](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day5)
+* [Day 6](https://github.com/thunguyen19/advent-of-code-2020/tree/master/day6)
--- day6/day6.py
+++ day6/day6.py
@@ -1,38 +1,59 @@
-def get_input_by_group(file_name):
+def get_input(file_name):
     store = []
-    information = ''
+    group = []
     with open(file_name, 'r') as file:
         lines = file.readlines()
         for line in lines:
-            line = line.strip() + ' '
-            if line == ' ':
-                store.append(information.strip())
-                information = ''
+            line = line.strip()
+            if line == '':
+                store.append(group)
+                group = []
             else:
-                information += line
-    if information:
-        store.append(information.strip())
+                group.append(line)
+    if group:
+        store.append(group)
     return store
 
 
-def massage_input(data):
-    store = []
-    for string in data:
-        store.append(string.replace(' ', ''))
+# PART 1
+def num_questions_anyone_answer(data):
+    result = 0
+    for group in data:
+        string = ''
+        for person in group:
+            string += person
+        result += len(set(string))
+    return result
+
+
+# PART 2
+def store_answer_per_group(group):
+    store = {}
+    for member in group:
+        for i in range(len(member)):
+            answer = member[i]
+            if store.get(answer):
+                store[answer] += 1
+            else:
+                store[answer] = 1
     return store
 
 
-def part_1(data):
-    count = 0
-    data = massage_input(data)
-    for answer in data:
-        count += len(set(answer))
-    return count
+def num_questions_everyone_answer(data):
+    result = 0
+    for group in data:
+        total_people = len(group)
+        store = store_answer_per_group(group)
+        for k, v in store.items():
+            if v == total_people:
+                result += 1
+    return result
 
 
 def main():
-    data = get_input_by_group('input.txt')
-    print(part_1(data))
+    data = get_input('input.txt')
+    assert num_questions_anyone_answer(data) == 6596
+    assert num_questions_everyone_answer(data) == 3219
 
 
 main()
